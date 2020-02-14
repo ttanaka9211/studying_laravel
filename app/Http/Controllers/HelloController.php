02e060ea53f2e49fd18128f8f5d9e8598fbe14a9
田中利幸 <ttanaka@hatchdogs.net>
2020-02-15 06:40:58 +0900

    リスト４−２３

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\HelloRequest;
+use Validator;
 
 class HelloController extends Controller
 {
@@ -14,6 +15,17 @@ class HelloController extends Controller
 
     public function post(HelloRequest $request)
     {
+        $validator = Validator ::make($request->all(), [
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => 'numeric|between:0,150',
+        ]);
+        if ($validator->fails()) {
+            return redirect('/hello')
+                ->withErrors($validator)
+                ->withInput();
+        }
+
         return view('hello.index', ['msg' => '正しく入力されました！']);
     }
 }
