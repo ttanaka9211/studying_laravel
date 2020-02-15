5de421be145fc7137e673bc152b28263f34ed26f
田中利幸 <ttanaka@hatchdogs.net>
2020-02-15 15:27:04 +0900

    リスト４−３３

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -14,35 +14,15 @@
     <table>
         <form action="/hello" method="post">
             {{csrf_field()}}
-            @if ($errors->has('name'))
-            <tr>
-                <th>ERROR</th>
-                <td>{{$errors->first('name')}}</td>
-            </tr>
-            @endif
-            <tr>
-                <th>name:</th>
-                <td><input type="text" name="name" value="{{old('name')}}"></td>
-            </tr>
-            @if ($errors->has('mail'))
+            @if ($errors->has('msg'))
                 <tr>
                     <th>ERROR</th>
-                    <td>{{$errors->first('mail')}}</td>
+                    <td>{{$errors->first('msg')}}</td>
                 </tr>
             @endif
             <tr>
-                <th>mail:</th>
-            <td><input type="text" name="mail" value="{{old('mail')}}"></td>
-            </tr>
-            @if ($errors->has('age'))
-                <tr>
-                    <th>ERROR</th>
-                    <td>{{$errors->first('age')}}</td>
-                </tr>
-            @endif
-            <tr>
-                <th>age:</th>
-                <td><input type="text" name="age" value="{{old('age')}}"></td>
+                <th>Message:</th>
+            <td><input type="text" name="msg" value="{{old('msg')}}"></td>
             </tr>
             <tr>
                 <th></th>
