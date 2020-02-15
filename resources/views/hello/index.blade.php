53da691f67cb140016a108ee39fb3ef63811d242
田中利幸 <ttanaka@hatchdogs.net>
2020-02-15 16:54:06 +0900

    リスト５−５

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -7,28 +7,19 @@
 @endsection
 
 @section('content')
-    <p>{{$msg}}</p>
-    @if (count($errors) > 0)
-    <p>入力に問題があります。再入力して下さい。</p>
-    @endif
     <table>
-        <form action="/hello" method="post">
-            {{csrf_field()}}
-            @if ($errors->has('msg'))
-                <tr>
-                    <th>ERROR</th>
-                    <td>{{$errors->first('msg')}}</td>
-                </tr>
-            @endif
+        <tr>
+            <th>Name</th>
+            <th>Mail</th>
+            <th>Age</th>
+        </tr>
+        @foreach ($items as $item)
             <tr>
-                <th>Message:</th>
-            <td><input type="text" name="msg" value="{{old('msg')}}"></td>
+                <td>{{$item->name}}</td>
+                <td>{{$item->mail}}</td>
+                <td>{{$item->age}}</td>
             </tr>
-            <tr>
-                <th></th>
-                <td><input type="submit" value="send"></td>
-            </tr>
-        </form>
+        @endforeach
     </table>
 @endsection
 
