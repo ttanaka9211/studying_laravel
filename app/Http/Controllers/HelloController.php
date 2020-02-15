a0b7407f8ca3234ddf8e48ec57b423164b67b31a
田中利幸 <ttanaka@hatchdogs.net>
2020-02-15 20:22:54 +0900

    エラー解消

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,21 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-<<<<<<< HEAD
 use App\Http\Requests\HelloRequest;
-=======
-use app\Http\Requests\HelloRequest;
->>>>>>> cd2f1cb77871223e7afea36df989212204f86f11
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
-<<<<<<< HEAD
-        return view('hello.index', ['msg' => 'フォームを入力ください。']);
-=======
         return view('hello.index', ['msg' => 'フォームを入力して下さい。']);
->>>>>>> cd2f1cb77871223e7afea36df989212204f86f11
     }
 
     public function post(HelloRequest $request)
