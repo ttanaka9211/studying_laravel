30517aff72765f52b0c477c4aa9a0e05b97adea9
田中利幸 <ttanaka@hatchdogs.net>
2020-02-15 16:48:08 +0900

    リスト５−４

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,31 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        if ($request->hasCookie('msg')) {
-            $msg = 'Cookie: '.$request->cookie('msg');
-        } else {
-            $msg = '*クッキーはありません。';
-        }
+        $items = DB::select('select * from people');
 
-        return view('hello.index', ['msg' => $msg]);
-    }
-
-    public function post(Request $request)
-    {
-        $validate_rule = [
-            'msg' => 'required',
-        ];
-        $this->validate($request, $validate_rule);
-        $msg = $request->msg;
-        $response = new Response(view('hello.index', ['msg' => '「'.$msg.'」をクッキに保存しました。']));
-        $response->cookie('msg', $msg, 100);
-
-        return $response;
+        return view('hello.index', ['items' => $items]);
     }
 }
