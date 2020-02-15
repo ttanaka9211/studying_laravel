b5315c6943efbf8faab784de9b6f9362cd0d86dc
田中利幸 <ttanaka@hatchdogs.net>
2020-02-15 23:58:34 +0900

    リスト５−１０

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ Route::get('/', function () {
 
 Route::get('hello', 'HelloController@index');
 Route::post('hello', 'HelloController@post');
+Route::get('hello/add','HelloController@add');
+Route::get('hello/add','HelloController@create')
