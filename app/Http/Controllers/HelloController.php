8182ad454049f68c3777b0ee46390bf03ae1ddb6
田中利幸 <ttanaka@hatchdogs.net>
2020-02-15 23:55:06 +0900

    リスト５ー９

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -18,4 +18,21 @@ class HelloController extends Controller
 
         return view('hello.index', ['items' => $items]);
     }
+
+    public function add(Request $request)
+    {
+        return view('hello.add');
+    }
+
+    public function create(Request $request)
+    {
+        $param = [
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+        ];
+        DB::insert('insert into people (name,mail,age) values(:name, :mail, :age)', $param);
+
+        return redirect('/hello');
+    }
 }
