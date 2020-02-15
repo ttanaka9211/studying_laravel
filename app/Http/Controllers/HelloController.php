219bd5829f8bf9d3e02917cac83e573f92681274
田中利幸 <ttanaka@hatchdogs.net>
2020-02-15 15:43:47 +0900

    リスト４−３４

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,20 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Validator;
+use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        $validator = Validator::make($request->query(), [
-            'id' => 'required',
-            'pass' => 'required',
-        ]);
-        if ($validator->fails()) {
-            $msg = 'クエリーに問題があります。';
+        if ($request->hasCookie('msg')) {
+            $msg = 'Cookie: '.$request->cookie('msg');
         } else {
-            $msg = 'ID/PASSを受け付けました。フォームを入力して下さい。';
+            $msg = '*クッキーはありません。';
         }
 
         return view('hello.index', ['msg' => $msg]);
@@ -24,31 +20,14 @@ class HelloController extends Controller
 
     public function post(Request $request)
     {
-        $rules = [
-            'name' => 'required',
-            'mail' => 'email',
-            'age' => 'numeric|between:0,150',
+        $validate_rule = [
+            'msg' => 'required',
         ];
-        $messages = [
-            'name.required' => '名前は必ず入力してください。',
-            'mail.email' => 'メールアドレスが必要です。',
-            'age.numeric' => '年齢を整数で入力して下さい。',
-            'age.between' => '年齢は０〜１５０の間で入力して下さい。',
-        ];
-        $validator = Validator ::make($request->all(), $rules, $messages);
-        $validator->sometimes('age', 'min:0', function ($input) {
-            return !is_int($input->age);
-        });
-        $validator->sometimes('age', 'max:200', function ($input) {
-            return !is_int($input->age);
-        });
+        $this->validate($request, $validate_rule);
+        $msg = $request->msg;
+        $response = new Response(view('hello.index', ['msg' => '「'.$msg.'」をクッキに保存しました。']));
+        $response->cookie('msg', $msg, 100);
 
-        if ($validator->fails()) {
-            return redirect('/hello')
-                ->withErrors($validator)
-                ->withInput();
-        }
-
-        return view('hello.index', ['msg' => '正しく入力されました！']);
+        return $response;
     }
 }
