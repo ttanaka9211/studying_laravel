cd2f1cb77871223e7afea36df989212204f86f11
田中利幸 <ttanaka@hatchdogs.net>
2020-02-15 15:06:55 +0900

    文字修正

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests\HelloRequest;
+use app\Http\Requests\HelloRequest;
 
 class HelloController extends Controller
 {
