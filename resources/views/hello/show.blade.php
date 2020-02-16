5e95a674d4a2716121c06b2b9af661b115f89a94
田中利幸 <ttanaka@hatchdogs.net>
2020-02-16 16:16:13 +0900

    エラー修正

--- resources/views/hello/show.blade.php
+++ resources/views/hello/show.blade.php
@@ -8,7 +8,25 @@
 @endsection
 
 @section('content')
-    @if ($items != null)
+<table>
+    <tr>
+        <th>id;</th>
+        <td>{{$item->id}}</td>
+    </tr>
+    <tr>
+        <th>name;</th>
+        <td>{{$item->name}}</td>
+    </tr>
+    <tr>
+        <th>mail;</th>
+        <td>{{$item->mail}}</td>
+    </tr>
+    <tr>
+        <th>age;</th>
+        <td>{{$item->age}}</td>
+    </tr>
+</table>
+{{--    @if ($items != null)
     @foreach($items as $item)
     <table width="400px">
         <tr>
@@ -21,7 +39,7 @@
         </tr>
     </table>
     @endforeach
-    @endif
+    @endif --}}
 @endsection
 
 @section('footer')
