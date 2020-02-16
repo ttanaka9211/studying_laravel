9f6d418b9f9c0837455c955ed873e9e59445d173
田中利幸 <ttanaka@hatchdogs.net>
2020-02-16 17:40:51 +0900

    リスト５−２５

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,7 +9,7 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        $items = DB::table('people')->get();
+        $items = DB::table('people')->orderBy('age', 'asc')->get();
 
         return view('hello.index', ['items' => $items]);
     }
