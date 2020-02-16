e54fff174242f1114ddb417eaf3a31c6d204cf3f
田中利幸 <ttanaka@hatchdogs.net>
2020-02-16 17:45:55 +0900

    リスト５−２６

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -33,11 +33,11 @@ class HelloController extends Controller
 
     public function show(Request $request)
     {
-        $min = $request->min;
-        $max = $request->max;
+        $page = $request->page;
         $items = DB::table('people')
-        ->whereRaw('age >= ? and age <= ?',
-        [$min, $max])->get();
+        ->offset($page * 3)
+        ->limit(3)
+        ->get();
 
         return view('hello.show', ['items' => $items]);
     }
