c408a09761ac4820716c8842f01e61bb9dd8d91a
田中利幸 <ttanaka@hatchdogs.net>
2020-02-16 22:48:56 +0900

    リスト５−１１

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.hello')
+@section('title','Edit')
+
+@section('menubar')
+    @parent
+    更新ページ
+@endsection
+
+@section('content')
+    <table>
+        <form action="/hello/edit" method="post">
+            @csrf
+            <input type="hidden" name="id" value="{{$from->id}}">
+            <tr>
+                <th>name;</th>
+                <td><input type="text" name="name"  value="{{$from->name}}"></td>
+            </tr>
+            <tr>
+                <th>mail;</th>
+                <td><input type="text" name="mail" value="{{$from->mail}}"></td>
+            </tr>
+            <tr>
+                <th>age;</th>
+                <td><input type="text" name="age" value="{{$form->age}}"></td>
+            </tr>
+            <tr>
+                <th></th>
+                <td><input type="submit" value="send"></td>
+            </tr>
+        </form>
+    </table>
+@endsection
+
+@section('footer')
+    copyright 2017 tuyano
+@endsection
