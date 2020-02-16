f5a3d1ec763a3266d475d6dd5128b6853f50914b
田中利幸 <ttanaka@hatchdogs.net>
2020-02-16 16:43:45 +0900

    リスト５−２２

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -34,8 +34,8 @@ class HelloController extends Controller
     public function show(Request $request)
     {
         $id = $request->id;
-        $item = DB::table('people')->where('id', $id)->first();
+        $items = DB::table('people')->where('id', '<=', $id)->get();
 
-        return view('hello/show', ['item' => $item]);
+        return view('hello.show', ['item' => $items]);
     }
 }
