46b4e781a8080191bcd6684fc9915d27cb5b2447
田中利幸 <ttanaka@hatchdogs.net>
2020-02-18 21:53:39 +0900

    リスト６−４

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,4 @@ Route::post('hello/del', 'HelloController@del');
 Route::get('hello/show', 'HelloController@show');
 Route::get('hello/edit', 'HelloController@edit');
 Route::post('hello/edit', 'HelloController@update');
+Route::get('person', 'PersonController@index');
