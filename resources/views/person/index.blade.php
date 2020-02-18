f5b4d48f2769c376234d017c2aca0201a5d94f02
田中利幸 <ttanaka@hatchdogs.net>
2020-02-18 21:52:19 +0900

    リスト６−３

---
+++ resources/views/person/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.hello')
+@section('title','Person.index')
+
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
+
+@section('content')
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Mail</th>
+            <th>Age</th>
+        </tr>
+        @foreach ($items as $item)
+            <tr>
+                <td>{{$item->name}}</td>
+                <td>{{$item->mail}}</td>
+                <td>{{$item->age}}</td>
+            </tr>
+        @endforeach
+    </table>
+@endsection
+
+@section('footer')
+    copylight 2017 tuyano.
+@endsection
