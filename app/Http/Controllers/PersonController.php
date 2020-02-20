39ce7ba1c5d6adb4ba8c496c9c6987a75cde182a
田中利幸 <ttanaka@hatchdogs.net>
2020-02-20 22:35:24 +0900

    リスト６−２０

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -29,4 +29,20 @@ class PersonController extends Controller
 
         return view('person.find', $param);
     }
+
+    public function add(Request $request)
+    {
+        return view('Person.add');
+    }
+
+    public function create(Request $request)
+    {
+        $this->validate($request, Person::$rules);
+        $person = new Person();
+        $form = $request->all();
+        unset($form['_token']);
+        $person->fill($form)->save();
+
+        return redirect('/person');
+    }
 }
