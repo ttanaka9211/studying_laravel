af0d300f2d0b75202cd59abc1407ff8395bd9c03
田中利幸 <ttanaka@hatchdogs.net>
2020-02-21 23:01:45 +0900

    6-42

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -14,7 +14,7 @@ class BoardController extends Controller
      */
     public function index(Request $request)
     {
-        $items = Board::all();
+        $items = Board::with('person')->get();
 
         return view('board.index', ['items' => $items]);
     }
