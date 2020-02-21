1c4e87d092d755e16212adf5e23e5fde72c58c21
田中利幸 <ttanaka@hatchdogs.net>
2020-02-21 22:29:10 +0900

    6-39

--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -12,8 +12,13 @@
         @foreach ($items as $item)
             <tr>
                 <td>{{$item->getData()}}</td>
-                <td>@if ($item->board !=null)
-                    {{$item->board->getData()}}
+                <td>
+                    @if ($item->boards !=null)
+                    <table width='100%'>
+                        @foreach ($item->boards as $obj)
+                            <tr><td>{{$obj->getData()}}</td></tr>
+                        @endforeach
+                    </table>
                 @endif
                 </td>
             </tr>
