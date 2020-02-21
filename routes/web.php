12270389eeccdd37b7da17e157056827dfe7c7f6
田中利幸 <ttanaka@hatchdogs.net>
2020-02-21 18:08:37 +0900

    6-34

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,5 @@ Route::post('person/edit', 'PersonController@update');
 Route::get('person/del', 'PersonController@delete');
 Route::post('person/del', 'PersonController@remove');
 Route::get('board', 'BoardController@index');
+Route::get('board/add', 'BoardController@add');
+Route::post('board/add', 'BoardController@create');
